e5c4181b39c54378caa694bfacc0fe8d1895d039
starlit-nox <kdkelly.1998@gmail.com>
2023-07-06 11:44:04 -0500

    Notes for Class 02 401
    Notes

---
+++ Numbers-Game/Numbers-Game/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Numbers_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my game! Let's do some math!");
+
+        }
+    }
+}
+
+// writeline
+// loop
+// writeline
+//number loop??
+// repeat this 6 times
+// random.math??
+//mutiply all given nubmers together and output product
+// 
